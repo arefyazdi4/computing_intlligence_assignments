d65ad64b6cb435380999a2cdfed64c6ace60f042
RF_BURGER <arefyazdi4@gmail.com>
2022-05-08 11:09:18 +0430

    predict sample review

--- project_3.py
+++ project_3.py
@@ -13,13 +13,13 @@ def vectorize_sequences(sequences, dimension=10000):
 if __name__ == '__main__':
     # loading limited number of word
     (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
-    print(train_data[0])
+    print(train_data[1])
     # decode on of reviews
     word_index = imdb.get_word_index()
     reverse_word_index = dict(
         [(value, key) for (key, value) in word_index.items()])
     decoded_review = ' '.join(
-        [reverse_word_index.get(i - 3, '?') for i in train_data[0]])  # ? if value couldn't be find
+        [reverse_word_index.get(i - 3, '?') for i in train_data[1]])  # ? if value couldn't be find
     print(decoded_review)
     # Encoding the integer sequences via one-hot encoding
     x_train = vectorize_sequences(train_data)
@@ -46,3 +46,32 @@ if __name__ == '__main__':
     model.fit(x_train, y_train, epochs=4, batch_size=512)
     results = model.evaluate(x_test, y_test)
     print(results)
+
+    sample_review = ['the film were great it was just brilliant so much that i bought the film as soon as it was ' \
+                     'released for and would recommend it to everyone to watch and the fly fishing was amazing really ' \
+                     'cried at the end it was so sad and you know what they say if you cry at a film it must have been ' \
+                     'good and this definitely was also to the two little boy that played the of norman and paul they ' \
+                     'were just brilliant children are often left out of the list i think because the stars that play ' \
+                     'them all grown up are such a big profile for the whole film but these children are amazing and ' \
+                     'should be praised for what they have done you think the whole story was so lovely because it was ' \
+                     'true and was someone life after all that was shared with ',
+                     'this terrible movie i love cheesy horror movies and seen hundreds but this had got to be on of '
+                     'the worst ever made the plot is paper thin and ridiculous the acting is an abomination the '
+                     'script is completely laughable the best is the end showdown with the cop and how he worked out '
+                     'who the killer is just so damn terribly written the clothes are sickening and funny in equal '
+                     'the hair is big lots of boobs men wear those cut shirts that show off their sickening that men '
+                     'actually wore them and the music is just trash that plays over and over again in almost every '
+                     'scene there is trashy music boobs and taking away bodies and the gym still close for all joking '
+                     'aside this is a truly bad film whose only charm is to look back on the disaster that was the 80 '
+                     'and have a good old laugh at how bad everything was back then '
+                     ]
+
+    sample_decode = [[], []]
+    for index, review in enumerate(sample_review):
+        sample_decode[index] = [word_index.get(word, -1)+3 for word in review]
+        sample_decode[index].insert(0, 1)
+
+    sample_vectorize = vectorize_sequences(sample_decode)
+
+    predict = model.predict(sample_vectorize)
+    print(predict)
