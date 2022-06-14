05b172670495fa88070d39f164d36f9aa00464a5
RF_BURGER <arefyazdi4@gmail.com>
2022-06-14 11:34:15 +0430

    Documenting boston housing

--- project_5_housing.py
+++ project_5_housing.py
@@ -10,8 +10,12 @@ def build_model():
         keras.layers.Dense(64, activation='relu'),
         keras.layers.Dense(64, activation='relu'),
         keras.layers.Dense(1)
+        # no activation (it will be a linear layer)
     ])
     model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+    # A metric is a function that is used to judge the performance of your model.
+    # Metric functions are similar to loss functions, except that
+    # the results from evaluating a metric are not used when training the model.
     return model
 
 
@@ -31,40 +35,22 @@ if __name__ == '__main__':
     print(train_data.shape)
     print(test_data.shape)
     # Normalizing the data
+    # you subtract the mean of the feature and divide by the standard deviation
+    # so that the feature is centered around 0 and has a unit standard deviation
+    # quantities used for normalizing the test data are computed using the training data.
     mean = train_data.mean(axis=0)
-    train_data -= mean
     std = train_data.std(axis=0)
+    train_data -= mean
     train_data /= std
     test_data -= mean
     test_data /= std
-    # K-fold validation
+    # K-fold cross-validation
+    # validation scores might change a lot depending on which data points
+    # you chose to use for validation and which you chose for training
+    # This would prevent you from reliably evaluating your model.
+    # The validation score for the model used is then the average of the validation scores obtained
     k = 4
     num_val_samples = len(train_data) // k
-    num_epochs = 100
-    all_scores = []
-    all_mae_histories = []
-    # for i in range(k):
-    #     print('processing fold #%d' % i)
-    #     val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
-    #     val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
-    #     partial_train_data = np.concatenate(
-    #         [train_data[:i * num_val_samples],
-    #          train_data[(i + 1) * num_val_samples:]],
-    #         axis=0)
-    #     partial_train_targets = np.concatenate(
-    #         [train_targets[:i * num_val_samples],
-    #          train_targets[(i + 1) * num_val_samples:]],
-    #         axis=0)
-    #     model = build_model()
-    #     history = model.fit(partial_train_data, partial_train_targets,
-    #               epochs=num_epochs, batch_size=1, verbose=0)
-    #     val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
-    #     all_scores.append(val_mae)
-    #
-    #     mae_history = history.history['val_mae']
-    #     all_mae_histories.append(mae_history)
-    # print(all_scores)
-    # print(np.mean(all_scores))
     num_epochs = 500
     all_mae_histories = []
     for i in range(k):
